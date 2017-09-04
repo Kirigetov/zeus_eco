3a4a631fb55d622b05277d85385a47ebc2b9d927
HOME_PC\Максим <maximkirigetov@gmail.com>
2017-09-05 00:50:37 +0300

    upd page

--- build/js/common.js
+++ build/js/common.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 	    $("#play-video.video").click(function(e){
 	    	e.preventDefault();
 
-	        $('#head').addClass('is-transparent');
+	        $('#team').addClass('is-transparent');
 
 	        player = new YT.Player('player', {
 	            width : '100%',
@@ -35,8 +35,55 @@ $(document).ready(function() {
     function onPlayerStateChange(event) {
         if(event.data == YT.PlayerState.ENDED) {
             player.destroy();
-            $('#head').removeClass('is-transparent');
+            $('#team').removeClass('is-transparent');
         }
     }
 
+    $('.js-nav-link').on('click', function() {
+        var section = $(this).attr('href');
+        
+        $('html, body').animate({
+            scrollTop: $(section).offset().top - 10
+        }, 500);
+
+        $('.js-nav-btn').removeClass('is-active');
+
+        return false;
+    });
+
+    $('.js-join-btn').on('click', function() {
+        var popup = $('html, body').find('.popup');
+        popup.addClass('is-open');
+        $('body').addClass('popup-open');
+    });
+
+    $('.js-close-popup').on('click', function() {
+    	var popup = $('html, body').find('.popup');
+        popup.removeClass('is-open');
+        $('body').removeClass('popup-open');
+    });
+
+    // scroll button
+    $(".js-scroll-top").on("click", function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 100);
+        return false;
+    });
+
+    var lastScrollTop = 0;
+    $(window).scroll(function(event){
+       var st = $(this).scrollTop();
+       if (st > lastScrollTop){
+            if (!$(".js-scroll-top").hasClass("is-visible")) {
+                $(".js-scroll-top").addClass("is-visible");
+            }
+
+       } else {
+        if ($(".js-scroll-top").hasClass("is-visible")) {
+            $(".js-scroll-top").removeClass("is-visible");
+        }
+       }
+       lastScrollTop = st;
+    });
 });
